cf2b7a7cf55eb7d75f7e1cb0fc1174c7678db39e
Homma <A4048744@astellas.com>
2025-05-14 13:48:56 +0900

    Update.

---
+++ R/BayesDecisionProbBinary.R
@@ -0,0 +1,119 @@
+#' Calculate the Go, NoGo and Gray probabilities for a clinical trial when outcome is binary
+#' under the Bayesian framework using two metrics: 
+#' (i)  posterior probability for the treatment effect to be greater than a threshold, and 
+#' (ii) posterior predictive probability of phase III study success.
+#'
+#' The function can obtain:
+#' \itemize{
+#'   \item Go probability
+#'   \item NoGo probability
+#'   \item Gray probability
+#' }
+#' The function can be used for controlled design, uncontrolled design and design using an external (historical) data.
+#'
+#' @param prob A type of probability (i.e., \code{prob='posterior'} or \code{prob='predictive'}).
+#' @param design A type of design (i.e., \code{design='controlled'}, \code{design='uncontrolled'} or \code{design='external'}).
+#' @param theta.TV A numeric pre-specified threshold value for calculating Go probability when \code{prob='posterior'}.
+#' @param theta.MAV A numeric pre-specified threshold value for calculating NoGo probability when \code{prob='posterior'}.
+#' @param theta.NULL A numeric pre-specified threshold value for calculating Go/NoGo probabilities when \code{prob='predictive'}.
+#' @param gamma1 A numeric value of a minimum probability to declare success.
+#' @param gamma2 A numeric value of a futility threshold. 
+#' @param pi1 A numeric value or a vector representing true response probability(s) for group 1. 
+#' @param pi2 A numeric value or a vector representing true response probability(s) for group 2.
+#' @param n1 A number of patients in group 1 for a proof-of-concept (PoC) trial.
+#' @param n2 A number of patients in group 2 for the PoC trial.
+#' @param a1 The first non-negative numeric value of the prior distribution for group 1.
+#' @param a2 The first non-negative numeric value of the prior distribution for group 2.
+#' @param b1 The second non-negative numeric value of the prior distribution for group 1.
+#' @param b2 The second non-negative numeric value of the prior distribution for group 2.
+#' @param z A hypothetical observed number of responders in group 2 for an uncontrolled design.
+#' @param m1 A number of patients in group 1 for the future trial data.
+#' @param m2 A number of patients in group 2 for the future trial data.
+#' @param ne1 A number of patients in group 1 for the external data.
+#' @param ne2 A number of patients in group 2 for the external data.
+#' @param ye1 An observed number of responders in group 1 for the external data.
+#' @param ye2 An observed number of responders in group 2 for the external data.
+#' @param ae1 A scale parameter (power parameter) for group 1.
+#' @param ae2 A scale parameter (power parameter) for group 2.
+#'
+#' @return The \code{BayesDecisionProbBinary} gives the dataframe including the
+#'  true response probabilities for both groups, and the Go, NoGo and Gray probabilities.
+#'
+#' @examples
+#' BayesDecisionProbBinary(
+#'   prob = 'posterior', design = 'controlled', theta.TV = 0.4, theta.MAV = 0.2, theta.NULL = NULL, gamma1 = 0.5, gamma2 = 0.2, 
+#'   pi1 = c(0.2, 0.4, 0.6, 0.8), pi2 = rep(0.2, 4), n1 = 12, n2 = 12, a1 = 0.5, a2 = 0.5, b1 = 0.5, b2 = 0.5, z = NULL,
+#'   m1 = NULL, m2 = NULL, ne1 = NULL, ne2 = NULL, ye1 = NULL, ye2 = NULL, ae1 = NULL, ae2 = NULL
+#' )
+#' BayesDecisionProbBinary(
+#'   prob = 'predictive', design = 'controlled', theta.TV = NULL, theta.MAV = NULL, theta.NULL = 0, gamma1 = 0.9, gamma2 = 0.3, 
+#'   pi1 = c(0.2, 0.4, 0.6, 0.8), pi2 = rep(0.2, 4), n1 = 12, n2 = 12, a1 = 0.5, a2 = 0.5, b1 = 0.5, b2 = 0.5, z = NULL,
+#'   m1 = 30, m2 = 30, ne1 = NULL, ne2 = NULL, ye1 = NULL, ye2 = NULL, ae1 = NULL, ae2 = NULL
+#' )
+#' 
+#' @importFrom stats dbinom
+#' @export
+BayesDecisionProbBinary = function(prob, design, theta.TV, theta.MAV, theta.NULL, gamma1, gamma2,
+                                   pi1, pi2, n1, n2, a1, a2, b1, b2, z,
+                                   m1, m2, ne1, ne2, ye1, ye2, ae1, ae2) {
+  # Check parameter sets
+  if((prob == 'posterior') & (sum(sapply(list(theta.TV, theta.MAV), is.null)) > 0)) {
+    stop('If you calculate the Go, NoGo and Gray probabilities using posterior probability, theta.TV and theta.MAV should be non-null')
+  }
+  if((prob == 'predictive') & (sum(sapply(list(m1, m2), is.null)) > 0)) {
+    stop('If you calculate the Go, NoGo and Gray probabilities using posterior predictive probability, m1 and m2 should be non-null')
+  }
+  if((prob == 'predictive') & (is.null(theta.NULL))) {
+    stop('If you calculate the Go, NoGo and Gray probabilities using posterior predictive probability, theta.NULL should be non-null')
+  }
+  if((design == 'uncontrolled') & (is.null(z))) {
+    stop('If you consider uncontrolled design, z should be non-null')
+  }
+  if((design == 'uncontrolled') & (is.null(z))) {
+    stop('If you consider uncontrolled design, z should be non-null')
+  }
+  if((design == 'external') & (sum(sapply(list(ne1, ne2, ye1, ye2, ae1, ae2), is.null)) > 0)) {
+    stop('If you use the external data, ne1, ne2, ye1, ye2, ae1 and ae2 should be non-null')
+  }
+  # Set trial design
+  external = ifelse(design == 'external', TRUE, FALSE)
+  # Set values of theta0
+  if(prob == 'posterior') { 
+    theta0 = c(theta.TV, theta.MAV)
+  } else { 
+    theta0 = c(theta.NULL, theta.NULL)
+  }
+  # Calculate bayesian posterior probability or bayesian posterior predictive probability
+  Y1 = 0:n1
+  if(design == 'uncontrolled') { Y2 = z } else { Y2 = 0:n2 }
+  # Go/NoGo probabilities
+  GoNogoProb = sapply(seq(2), function(i) {
+    gPost = sapply(Y1, function(y1) {
+      sapply(Y2, function(y2) {
+        BayesPostPredBinary(
+          prob, external, theta0[i], 
+          n1, n2, y1, y2, a1, a2, b1, b2, 
+          m1, m2, ne1, ne2, ye1, ye2, ae1, ae2
+        )
+      })
+    })
+    I = if(i == 1) {
+      matrix((gPost >= gamma1), nrow = length(Y2))
+    } else if(i == 2) {
+      matrix((gPost <= gamma2), nrow = length(Y2))
+    }
+    GoProb = diag(crossprod(
+      outer(col(I)[I] - 1, pi1, FUN = function(X, Y) dbinom(X, n1, Y)),
+      outer(row(I)[I] - 1, pi2, FUN = function(X, Y) dbinom(X, n2, Y))
+    ))
+    return(GoProb)
+  })
+  GoNogoProb = matrix(GoNogoProb, ncol = 2)
+  # Gray probability
+  GrayProb = 1 - rowSums(GoNogoProb)
+  # Results
+  results = data.frame(
+    pi1, pi2, Go = GoNogoProb[, 1], Gray = GrayProb, NoGo = GoNogoProb[, 2]
+  )
+  return(format(results, scientific = F))
+}
---
+++ R/BayesPostPredBinary.R
@@ -0,0 +1,87 @@
+#' Calculate bayesian posterior probability or bayesian posterior predictive probability
+#' for a clinical trial when outcome is binary.
+#'
+#' The function can obtain:
+#' \itemize{
+#'   \item Bayesian posterior probability
+#'   \item Bayesian posterior predictive probability
+#' }
+#' Prior distribution of proportion of responders (pi_{j}) for each treatment group (j=1,2) is 
+#' following beta distribution.
+#' For posterior probability, posterior distribution of pi_{j} is following beta distribution.
+#' For posterior predictive probability, predictive distribution of future trial data is 
+#' following beta-binomial distribution.
+#' The function can account for external (historical) data.
+#'
+#' @param prob A type of probability (i.e., \code{prob='posterior'} or \code{prob='predictive'}).
+#' @param external When external data is used, \code{external=TRUE}.
+#' @param theta0 A numeric pre-specified threshold value.
+#' @param n1 A number of patients in group 1 for a proof-of-concept (PoC) trial.
+#' @param n2 A number of patients in group 2 for the PoC trial.
+#' @param y1 An observed number of responders in group 1 for the PoC trial.
+#' @param y2 An observed number of responders in group 2 for the PoC trial.
+#' @param a1 The first non-negative numeric value of the prior distribution for group 1.
+#' @param a2 The first non-negative numeric value of the prior distribution for group 2.
+#' @param b1 The second non-negative numeric value of the prior distribution for group 1.
+#' @param b2 The second non-negative numeric value of the prior distribution for group 2.
+#' @param m1 A number of patients in group 1 for the future trial data.
+#' @param m2 A number of patients in group 2 for the future trial data.
+#' @param ne1 A number of patients in group 1 for the external data.
+#' @param ne2 A number of patients in group 2 for the external data.
+#' @param ye1 An observed number of responders in group 1 for the external data.
+#' @param ye2 An observed number of responders in group 2 for the external data.
+#' @param ae1 A scale parameter (power parameter) for group 1.
+#' @param ae2 A scale parameter (power parameter) for group 2.
+#'
+#' @return The \code{BayesPostPredBinary} gives the numeric value of the 
+#' bayesian posterior probability or bayesian posterior predictive probability.
+#'
+#' @examples
+#' BayesPostPredBinary(
+#'   prob = 'posterior', external = TRUE, theta0 = 0.15, 
+#'   n1 = 12, n2 = 15, y1 = 7, y2 = 9, a1 = 0.5, a2 = 0.5, b1 = 0.5, b2 = 0.5, 
+#'   m1 = NULL, m2 = NULL, ne1 = 12, ne2 = 12, ye1 = 6, ye2 = 6, ae1 = 0.5, ae2 = 0.5
+#' )
+#' BayesPostPredBinary(
+#'   prob = 'predictive', external = TRUE, theta0 = 0.5, 
+#'   n1 = 12, n2 = 15, y1 = 7, y2 = 7, a1 = 0.5, a2 = 0.5, b1 = 0.5, b2 = 0.5, 
+#'   m1 = 12, m2 = 12, ne1 = 12, ne2 = 12, ye1 = 6, ye2 = 6, ae1 = 0.5, ae2 = 0.5
+#' )
+#' 
+#' @importFrom cubature adaptIntegrate
+#' @importFrom VGAM dbetabinom.ab
+#' @export
+BayesPostPredBinary = function(prob, external, theta0,
+                               n1, n2, y1, y2, a1, a2, b1, b2, 
+                               m1, m2, ne1, ne2, ye1, ye2, ae1, ae2) {
+  # Check parameter sets
+  if((prob == 'predictive') & (sum(sapply(list(m1, m2), is.null)) > 0)) {
+    stop('If you calculate posterior predictive probability, m1 and m2 should be non-null')
+  }
+  if((external == TRUE) & (sum(sapply(list(ne1, ne2, ye1, ye2, ae1, ae2), is.null)) > 0)) {
+    stop('If you use the external data, ne1, ne2, ye1, ye2, ae1 and ae2 should be non-null')
+  }
+  # Set shape parameters
+  s11 = y1 + a1 + external * c(ye1 * ae1, 0)[1]
+  s12 = y2 + a2 + external * c(ye2 * ae2, 0)[1]
+  s21 = n1 - y1 + b1 + external * c((ne1 - ye1) * ae1, 0)[1]
+  s22 = n2 - y2 + b1 + external * c((ne2 - ye2) * ae2, 0)[1]
+  # A posterior probability
+  if(prob == 'posterior') {
+    g = cubature::adaptIntegrate(
+      function(theta) ddiff2beta(theta, s11, s12, s21, s22),
+      lowerLimit = theta0,
+      upperLimit = 1
+    )[['integral']]
+    # A posterior predictive probability
+  } else if(prob == 'predictive') {
+    I = (outer(m2 * 0:m1, m1 * 0:m2, '-') / (m1 * m2) > theta0)
+    g = as.numeric(
+      crossprod(
+        VGAM::dbetabinom.ab(row(I)[I] - 1, m1, s11, s21),
+        VGAM::dbetabinom.ab(col(I)[I] - 1, m2, s12, s22)
+      )
+    )
+  }
+  return(g)
+}
