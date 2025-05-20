ddcbb8134e792ec50a7fcddc6d2265b28f666c0f
Homma <A4048744@astellas.com>
2025-05-20 12:14:50 +0900

    Updated (from simulation-based approach to numerical integration approach).

--- R/BayesPostPredContinuous.R
+++ R/BayesPostPredContinuous.R
@@ -9,7 +9,6 @@
 #' Prior distribution of mean and variance of outcomes for each treatment group (k=1,2) is (1) Normal-Inverse-X^2 or (2) Vague.
 #' Posterior distribution or posterior predictive distribution of outcome for each treatment group is following t-distribution.
 #'
-#' @param msim A numeric value of a number of data generation in PoC trial.
 #' @param design A type of trial design (i.e., \code{design='controlled'} or \code{prob='uncontrolled'}).
 #' @param prob A type of probability (i.e., \code{prob='posterior'} or \code{prob='predictive'}).
 #' @param prior A prior distribution (i.e., \code{prior='N-Inv-Chisq'} or \code{prior='vague'}).
@@ -31,28 +30,28 @@
 #' @param s1 A sample standard deviation of group 1.
 #' @param s2 A sample standard deviation of group 2.
 #' @param r A parameter value associated with the distribution of mean for group 2 When \code{prob='uncontrolled'}.
-#' @param seed A numeric value of a seed number for data generation.
 #'
-#' @return The \code{BayesPostPredContinuous} gives the numeric value of the
+#' @return The \code{BayesPostPredContinuous} gives the numeric value(s) of the
 #' bayesian posterior probability or bayesian posterior predictive probability.
 #'
 #' @examples
 #' BayesPostPredContinuous(
-#'   msim = 10000, design = 'controlled', prob = 'posterior', prior = 'N-Inv-Chisq', theta0 = c(2, 0), n1 = 12, n2 = 12, m1 = NULL, m2 = NULL,
+#'   design = 'controlled', prob = 'posterior', prior = 'N-Inv-Chisq', theta0 = c(2, 0), n1 = 12, n2 = 12, m1 = NULL, m2 = NULL,
 #'   kappa01 = 5, kappa02 = 5, nu01 = 5, nu02 = 5, mu01 = 5, mu02 = 5, sigma01 = sqrt(5), sigma02 = sqrt(5),
-#'   bar.y1 = 2, bar.y2 = 0, s1 = 1, s2 = 1, r = NULL, seed = 1
+#'   bar.y1 = 2, bar.y2 = 0, s1 = 1, s2 = 1, r = NULL
 #' )
 #' BayesPostPredContinuous(
-#'   msim = 10000, design = 'uncontrolled', prob = 'predictive', prior = 'vague', theta0 = 0.5, n1 = 12, n2 = NULL, m1 = 120, m2 = 120,
+#'   design = 'uncontrolled', prob = 'predictive', prior = 'vague', theta0 = 0.5, n1 = 12, n2 = NULL, m1 = 120, m2 = 120,
 #'   kappa01 = NULL, kappa02 = NULL, nu01 = NULL, nu02 = NULL, mu01 = NULL, mu02 = 0, sigma01 = NULL, sigma02 = NULL,
-#'   bar.y1 = 2, bar.y2 = NULL, s1 = 1, s2 = NULL, r = 12, seed = 1
+#'   bar.y1 = 2, bar.y2 = NULL, s1 = 1, s2 = NULL, r = 12
 #' )
 #'
-#' @importFrom stats rt
+#' @importFrom fGarch dstd pstd
+#' @importFrom cubature adaptIntegrate
 #' @export
-BayesPostPredContinuous = function(msim, design, prob, prior, theta0, n1, n2, m1, m2,
+BayesPostPredContinuous = function(design, prob, prior, theta0, n1, n2, m1, m2,
                                    kappa01, kappa02, nu01, nu02, mu01, mu02, sigma01, sigma02,
-                                   bar.y1, bar.y2, s1, s2, r, seed) {
+                                   bar.y1, bar.y2, s1, s2, r) {
   # Check parameter sets
   if((design == 'uncontrolled') & (is.null(r))) {
     stop('If you consider uncontrolled design, r should be non-null')
@@ -63,14 +62,12 @@ BayesPostPredContinuous = function(msim, design, prob, prior, theta0, n1, n2, m1
   if((prior == 'N-Inv-Chisq') & (sum(sapply(list(kappa01, nu01, sigma01), is.null)) > 0)) {
     stop('If you use the N-Inv-Chisq prior, kappa01, nu01 and sigma01 should be non-null')
   }
-  # Set seed number
-  set.seed(seed)
   # Indicator function that prior distribution is an N-Inv-Chisq
   I.NIC = rep(as.logical(prior == 'N-Inv-Chisq'), 2)
   # Indicator function that the type of probability is a posterior
-  I.POS = as.logical(prob == 'posterior')
+  I.POS = rep(as.logical(prob == 'posterior'), 2)
   # Renaming parameters
-  nk = c(n1, n2); mu0k = c(mu01, mu02); kappa0k = c(kappa01, kappa02); nu0k = c(nu01, nu02);
+  nk = c(n1, n2); mk = c(m1, m2); mu0k = c(mu01, mu02); kappa0k = c(kappa01, kappa02); nu0k = c(nu01, nu02);
   sigma0k = c(sigma01, sigma02); bar.yk = c(bar.y1, bar.y2); sk = c(s1, s2)
   # Sample size
   kappa.nk = nk + ifelse(I.NIC, kappa0k, 0)
@@ -83,19 +80,25 @@ BayesPostPredContinuous = function(msim, design, prob, prior, theta0, n1, n2, m1
     sk ^ 2 * ifelse(I.NIC, (nk - 1) / nu.nk, 1),
     ifelse(I.NIC, (nu0k * sigma0k ^ 2 + nk * kappa0k / kappa.nk * (mu0k - bar.yk) ^ 2) / nu.nk, 0)
   )
-  ## Generate t-distributed random variables
-  # Group 1
-  rand.t1 = sqrt(var.nk[1] / kappa.nk[1]) * ifelse(I.POS, 1, sqrt((1 + kappa.nk[1]) / m1)) * rt(msim, df = nu.nk[1], ncp = 0)
-  # Group 2
-  if(design == 'controlled') {
-    rand.t2 = sqrt(var.nk[2] / kappa.nk[2]) * ifelse(I.POS, 1, sqrt((1 + kappa.nk[2]) / m2)) * rt(msim, df = nu.nk[2], ncp = 0)
-    # Calculate theta values (= t_{df_{1}} - t_{df_{2}})
-    theta =  rand.t1 - rand.t2 + diff(rev(mu.nk))
-  } else if(design == 'uncontrolled') {
-    # Calculate theta values (= t_{df_{1}} - t_{df_{2}})
-    theta = ifelse(I.POS, sqrt(r + 1), sqrt(r * m1 / m2 + 1)) * rand.t1 + mu.nk[1] - mu02
-  }
+  # The standard deviations of t-distributions
+  sd.tk = sqrt(var.nk / kappa.nk) * ifelse(I.POS, 1, sqrt((1 + kappa.nk) / mk))
   # The probability of exceeding \theta_{0}
-  g = sapply(theta0, function(i) sum(c(theta) > i) / msim)
+  g = sapply(theta0, function(i) {
+    cubature::adaptIntegrate(
+      function(x, Theta0) {
+        t1 = fGarch::dstd(x, mean = mu.nk[1], sd = sd.tk[1], nu = nu.nk[1])
+        if(design == 'controlled') {
+          t2 = fGarch::pstd(x - Theta0, mean = mu.nk[2], sd = sd.tk[2], nu = nu.nk[2])
+        } else if(design == 'uncontrolled') {
+          t2 = fGarch::pstd(x - Theta0, mean = mu02, sd = sqrt(r) * sd.tk[1], nu = nu.nk[1])
+        }
+        return(t1 * t2)
+      },
+      lowerLimit = -Inf,
+      upperLimit = Inf,
+      Theta0 = i,
+      tol = 1e-4
+    )[['integral']]
+  })
   return(g)
 }
